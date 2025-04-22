ae786b05961ce51c962cc95af37d54ea2c192a28
mohammad.firmansyah <firmansmoh842@gmail.com>
2025-04-22 15:50:56 +0700

    fix: toast position not exist

--- toasify/src/main/java/com/mostrans/toasify/utils/ToastManager.kt
+++ toasify/src/main/java/com/mostrans/toasify/utils/ToastManager.kt
@@ -1,6 +1,7 @@
 package com.mostrans.toasify.utils
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
@@ -8,35 +9,58 @@ object ToastManager {
     private val _toastState = MutableStateFlow<ToastData?>(null)
     val toastState = _toastState.asStateFlow()
 
-    fun showInfo(title: String? = "Warning", message: String) {
-        _toastState.value = ToastData(title = title, message = message, type = ToastType.Info)
+    fun showInfo(
+        title: String? = "Info",
+        message: String,
+        position: ToastPosition? = null,
+    ) {
+        _toastState.value =
+            ToastData(title = title, message = message, type = ToastType.Info, position = position)
     }
 
-    fun showSuccess(title: String? = "Success", message: String) {
-        _toastState.value = ToastData(title = title, message = message, type = ToastType.Success)
+    fun showSuccess(
+        title: String? = "Success",
+        message: String,
+        position: ToastPosition? = null,
+    ) {
+        _toastState.value = ToastData(
+            title = title, message = message, type = ToastType.Success, position = position
+        )
     }
 
-    fun showError(title: String? = "Warning", message: String) {
-        _toastState.value = ToastData(title = title, message = message, type = ToastType.Error)
+    fun showError(
+        title: String? = "Error",
+        message: String,
+        position: ToastPosition? = null,
+    ) {
+        _toastState.value =
+            ToastData(title = title, message = message, type = ToastType.Error, position = position)
     }
 
     fun showWarning(
         title: String? = "Warning",
-        message: String,
+        message: String, position: ToastPosition? = null,
     ) {
-        _toastState.value = ToastData(title = title, message = message, type = ToastType.Warning)
+        _toastState.value = ToastData(
+            title = title, message = message, type = ToastType.Warning, position = position
+        )
     }
 
     fun showLoading(
         title: String = "Loading",
+        position: ToastPosition? = null,
     ) {
-        _toastState.value = ToastData(title = title, type = ToastType.Loading)
+        _toastState.value = ToastData(title = title, type = ToastType.Loading, position = position)
     }
 
     fun showCustom(
         content: @Composable () -> Unit,
+        position: ToastPosition? = null,
     ) {
-        _toastState.value = ToastData(content = content, type = ToastType.Custom)
+        _toastState.value = ToastData(
+            content = content, type = ToastType.Custom,
+            position = position
+        )
     }
 
     fun clearToast() {
@@ -47,10 +71,23 @@ object ToastManager {
 data class ToastData(
     val title: String? = "",
     val message: String? = "",
+    val position: ToastPosition? = ToastPosition.Top,
     val type: ToastType = ToastType.Default,
     val content: (@Composable () -> Unit)? = null,
 )
 
 enum class ToastType {
     Success, Error, Warning, Default, Loading, Custom, Info
+}
+
+enum class ToastPosition {
+    Top, Center, Bottom
+}
+
+fun ToastPosition.toAlignType(): Alignment {
+    return when (this) {
+        ToastPosition.Top -> Alignment.TopCenter
+        ToastPosition.Center -> Alignment.Center
+        ToastPosition.Bottom -> Alignment.BottomCenter
+    }
 }
